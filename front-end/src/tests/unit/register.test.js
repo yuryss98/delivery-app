e123260043a47bf5c86ce92321d1518599fdcb71
leandrojamir <leandrojamir@hotmail.com>
2023-03-13 18:12:24 -0300

    merge with main

--- front-end/src/tests/unit/register.test.js
+++ front-end/src/tests/unit/register.test.js
@@ -2,36 +2,65 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../helpers/renderWithRouter';
-import App from '../../App';
+import Register from '../../auth/register';
+// import { requestLogin } from '../../utils/apiConnection';
+// import { useNavigate } from 'react-router-dom';
 
-describe('Testing register screen', () => {
-  it('Test the elements are rendered', () => {
-    renderWithRouter(<App />, { route: '/register' });
+jest.mock('../../utils/apiConnection');
+jest.mock('react-router-dom', () => ({
+  useNavigate: jest.fn(),
+}));
 
-    const inputName = screen.getByTestId(/common_register__input-name/i);
-    const inputEmail = screen.getByTestId(/common_register__input-email/i);
-    const inputPass = screen.getByTestId(/common_register__input-password/i);
-    const registerBtn = screen.getByTestId(/common_register__button-register/i);
-
-    expect(inputName).toBeInTheDocument();
-    expect(inputEmail).toBeInTheDocument();
-    expect(inputPass).toBeInTheDocument();
-    expect(registerBtn).toBeInTheDocument();
+describe('Register Screen Page Tests:', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
-  it('Register button is disabled', () => {
-    renderWithRouter(<App />, { route: '/register' });
 
-    const inputName = screen.getByTestId(/common_register__input-name/i);
-    const inputEmail = screen.getByTestId(/common_register__input-email/i);
-    const inputPass = screen.getByTestId(/common_register__input-password/i);
-    const registerBtn = screen.getByTestId(/common_register__button-register/i);
+  it('should render all the input elements', () => {
+    renderWithRouter(<Register />, { route: '/register' });
 
-    // expect(loginBtn).toBeDisabled();
+    expect(screen.getByTestId('common_register__input-name')).toBeInTheDocument();
+    expect(screen.getByTestId('common_register__input-email')).toBeInTheDocument();
+    expect(screen.getByTestId('common_register__input-password')).toBeInTheDocument();
+    expect(screen.getByTestId('common_register__button-register')).toBeInTheDocument();
+  });
 
-    userEvent.type(inputName, 'lean');
-    userEvent.type(inputEmail, 'leandrojamir@t');
-    userEvent.type(inputPass, '1234');
+  it('should disable the register button when input values are not valid', async () => {
+    renderWithRouter(<Register />, { route: '/register' });
+
+    const inputName = screen.getByTestId('common_register__input-name');
+    const inputEmail = screen.getByTestId('common_register__input-email');
+    const inputPassword = screen.getByTestId('common_register__input-password');
+    const registerBtn = screen.getByTestId('common_register__button-register');
 
     expect(registerBtn).toBeDisabled();
+
+    userEvent.type(inputName, 'invalid');
+    expect(registerBtn).toBeDisabled();
+
+    userEvent.type(inputEmail, 'invalid');
+    expect(registerBtn).toBeDisabled();
+
+    userEvent.type(inputPassword, 'invalid');
+    expect(registerBtn).toBeDisabled();
+
+    userEvent.type(inputName, 'Miguel Vieira');
+    expect(registerBtn).toBeDisabled();
+
+    userEvent.type(inputEmail, 'miguel@try');
+    expect(registerBtn).toBeDisabled();
+
+    userEvent.type(inputPassword, '123456');
+    expect(registerBtn).toBeDisabled();
+
+    userEvent.type(inputName, 'Mayara Marques');
+    userEvent.type(inputEmail, 'mayara@trybe.com');
+    userEvent.type(inputPassword, '12');
+    expect(registerBtn).toBeDisabled();
+
+    userEvent.type(inputName, 'Yury');
+    userEvent.type(inputEmail, 'yury@trybe.com');
+    userEvent.type(inputPassword, '123456');
+    expect(registerBtn).not.toBeDisabled();
   });
 });
